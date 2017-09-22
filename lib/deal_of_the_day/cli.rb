d7a842b0453713c542730131d96fa7e41d23aabf
Justin Tooley <jstooley823@gmail.com>
2017-09-22 03:36:42 +0000

    added basic idea of deals methods and menu method

--- lib/deal_of_the_day/cli.rb
+++ lib/deal_of_the_day/cli.rb
@@ -1,23 +1,34 @@
 class DealOfTheDay::CLI
   def call
+    
     while input.downcase != 'exit'
+      
       puts "Would you like to look at Amazons deals of the day? of Sears deals?(Type 'A')"
       puts "Or Sears deals of the day?(Type 'S')"
       puts "Type exit to quit."
+      
       input = gets.chomp
       if input.downcase == "a"
         amazon_deals
-        amazon_menu
+        menu(input)
       elsif input.downcase == "s"
         sears_deals
-        sears_menu
+        menu(input)
       else
         puts "Did not understand command."
       end
     end #while loop end
   end # call method end
 
-  def list_deals
+  def amazon_deals
+    puts "Amazons deals here"
+  end
+
+  def sears_deals
+    puts "Sears deals here"
+  end
+
+  def menu(site_choice)
   end
   
 end
