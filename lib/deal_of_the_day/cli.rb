7d45d5467fe8607030673ce59130fa4ec01cc2df
Justin Tooley <jstooley823@gmail.com>
2017-09-22 03:29:36 +0000

    added call method

--- lib/deal_of_the_day/cli.rb
+++ lib/deal_of_the_day/cli.rb
@@ -1,5 +1,23 @@
 class DealOfTheDay::CLI
   def call
-    puts "Deals of the day."
+    while input.downcase != 'exit'
+      puts "Would you like to look at Amazons deals of the day? of Sears deals?(Type 'A')"
+      puts "Or Sears deals of the day?(Type 'S')"
+      puts "Type exit to quit."
+      input = gets.chomp
+      if input.downcase == "a"
+        amazon_deals
+        amazon_menu
+      elsif input.downcase == "s"
+        sears_deals
+        sears_menu
+      else
+        puts "Did not understand command."
+      end
+    end #while loop end
+  end # call method end
+
+  def list_deals
   end
+  
 end
